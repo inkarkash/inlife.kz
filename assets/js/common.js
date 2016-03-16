b61661f978ed7bd839e896892fe32e3855be40a1
Sagynysh <sagynyshkenel@gmail.com>
2016-03-16 11:54:48 +0600

    Porfoltio modal and messanger work

--- assets/js/common.js
+++ assets/js/common.js
@@ -8,7 +8,12 @@ $(document).ready(function() {
 		}).done(function() {
 			$(this).find("input").val("");
 			$("#message").trigger("reset");
+			alert("Спасибо за вашу заявку, мы скоро свяжемся с вами!");
 			// $('#alertsuccess').show();
+		}).fail(function(){
+			$(this).find("input").val("");
+			$("#message").trigger("reset");
+			alert("Ошибка!!!");
 		});
 		return false;
 	});
--- assets/mail.php
+++ assets/mail.php
@@ -5,8 +5,9 @@ $sitename = "Inlife.kz";
 
 $email = trim($_POST["email"]);
 $phone = trim($_POST["phone"]);
-$message = trim($_POST["message"]);
-$message = "Email: $email \nТелефон: $phone \nСообщение: $message";
+$msg = trim($_POST["message"]);
+$message = "Email: $email \nТелефон: $phone \nСообщение: $msg";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+mail($recepient, $pagetitle, $message);
+?>
